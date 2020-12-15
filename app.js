ac1ed37cefb51f4832a6ec6c38979035b1b296db
okuninoshi <gennesis@outlook.fr>
2020-12-15 21:46:33 +0100

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const wikiRouter = require('./routes/wiki');
+// const wikiRouter = require('./routes/wiki');
 const catalogRouter = require('./routes/catalog');  //Import routes for "catalog" area of site
 const app = express();
 
@@ -31,8 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/wiki', wikiRouter);
 app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
+// app.use('/wiki', wikiRouter);
 // app.use('/author', authorRouter)
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
