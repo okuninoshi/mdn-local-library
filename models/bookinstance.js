c3ca6b907eb462fce178f4aa44cd7bcb80381f51
okuninoshi <gennesis@outlook.fr>
2020-12-17 22:39:51 +0100

    format due date value

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const BookInstanceSchema = new Schema(
     {
@@ -23,5 +23,10 @@ BookInstanceSchema
         return '/catalog/bookinstance/' + this._id;
     });
 
+BookInstanceSchema
+    .virtual('due_back_formatted')
+    .get(function () {
+        return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
+    });
 //Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
