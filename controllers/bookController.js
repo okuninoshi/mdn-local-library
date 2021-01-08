deab8e7235635c7f1a70591b284c14bda85404db
okuninoshi <gennesis@outlook.fr>
2021-01-08 15:27:21 +0100

    implement book_delete_get page

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -155,8 +155,23 @@ exports.book_create_post = [
 ];
 
 // Display book delete form on GET.
-exports.book_delete_get = function (req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+exports.book_delete_get = function (req, res, next) {
+    async.parallel({
+        book: function (callback) {
+            Book.findById(req.params.id).exec(callback)
+        },
+        book_instances: function (callback) {
+            BookInstance.find({ 'book': req.params.id }).exec(callback)
+        }
+    }, function (err, results) {
+        if (err) { return next(err); }
+        if (results.book == null) { // No results.
+            res.redirect('/catalog/books');
+        }
+        // Successful, so render.
+        res.render('book_delete', { title: 'Delete Book', book: results.book, book_instances: results.book_instances });
+        // console.log("book delete get :", results);
+    });
 };
 
 // Handle book delete on POST.
