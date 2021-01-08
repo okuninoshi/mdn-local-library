3a3b1d69e8b58aa4405eee6593647800d25e2191
okuninoshi <gennesis@outlook.fr>
2021-01-08 14:43:55 +0100

    implement the genre delete post page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -110,8 +110,32 @@ exports.genre_delete_get = function (req, res) {
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Genre delete POST');
+exports.genre_delete_post = function (req, res, next) {
+
+    async.parallel({
+        author: function (callback) {
+            Genre.findById(req.body.genreid).exec(callback)
+        },
+        genre_books: function (callback) {
+            Book.find({ 'genre': req.body.genreid }).exec(callback)
+        },
+    }, function (err, results) {
+        if (err) { return next(err); }
+        // Success
+        if (results.genre_books.length > 0) {
+            // Genre has books. Render in same way as for GET route.
+            res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books });
+            return;
+        }
+        else {
+            // Genre has no books. Delete object and redirect to the list of genres.
+            Genre.findByIdAndRemove(req.body.genreid, function deleteGenre(err) {
+                if (err) { return next(err); }
+                // Success - go to genre list
+                res.redirect('/catalog/genres')
+            })
+        }
+    });
 };
 
 // Display Genre update form on GET.
